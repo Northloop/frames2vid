fc57c72338720492b270d520aa9204b23dbbe4dd
Andro.Meta <Northloop@users.noreply.github.com>
2023-09-23 16:30:43 -0500

    Add files via upload

---
+++ frames 2 video.bat
@@ -0,0 +1,43 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Ask the user to drag and drop the folder
+echo Please drag and drop the folder onto this window and press Enter.
+set /p folderPath="Folder: "
+
+:: Remove quotes from the folder path
+set "folderPath=%folderPath:"=%"
+
+:: Check if it's a valid directory
+if not exist "%folderPath%\" (
+    echo The provided path is not a valid directory.
+    pause
+    exit /b
+)
+
+:: Ask for frames per second
+set /p fps="Enter the desired frames per second (e.g., 24): "
+
+:: Ask for the desired output video name
+set /p outputName="Enter the desired name for the output video (without extension): "
+
+:: Get the parent directory of the folderPath
+for %%i in ("%folderPath%") do set "parentDir=%%~dpi"
+
+:: Create a temporary list of all images
+set "tempList=%temp%\image_list.txt"
+if exist "%tempList%" del "%tempList%"
+
+for %%f in ("%folderPath%\*.png") do (
+    echo file '%%f' >> "%tempList%"
+)
+
+:: Create the video using the list
+ffmpeg -f concat -safe 0 -i "%tempList%" -r %fps% -c:v libx264 -pix_fmt yuv420p "%parentDir%\%outputName%.mp4"
+
+:: Clean up the temporary list
+del "%tempList%"
+
+echo.
+echo Video creation complete! The video is saved next to the folder.
+pause
